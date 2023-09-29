640371bea691695666d9443c67253490d969ca0a
Shana(Sana) <sanabarilad@gmail.com>
2023-09-29 12:44:58 +0200

    sidebar component and tailwind setup

---
+++ my-portfolio/src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export const Sidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col">
+      <i className="">Dark theme</i>
+      <i className="">Light theme</i>
+    </div>
+  );
+};
---
+++ my-portfolio/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Sidebar } from "../components/Sidebar";
+
+export const HomePage = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+    </div>
+  );
+};
