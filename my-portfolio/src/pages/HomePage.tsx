fbe3422eda071f20c399499e6f7c76acfa34523a
Sana <72435416+SanaIsShana@users.noreply.github.com>
2023-09-29 17:00:56 +0200

    Main page (#1)
    * sidebar component and tailwind setup
    
    * add animation background

---
+++ my-portfolio/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar";
+import { Text } from "../components/Banner";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Text />
+    </div>
+  );
+};
