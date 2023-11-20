6580f5fee8937838e822b1b82d3a709fe9270c66
Shana(Sana) <sanabarilad@gmail.com>
2023-11-20 19:16:05 +0100

    update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,13 @@ export default defineConfig({
   base: "/portfolio/",
   root: "./",
   build: {
-    outDir: "dist",
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
   },
   publicDir: "/public/assets",
 })
